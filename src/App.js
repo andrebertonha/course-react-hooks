89944c668afb75f6a7fd0ee17892c8e404adcd91
André Martins <andrebertonhadev@gmail.com>
2020-04-01 10:45:08 -0300

    styling components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.scss";
 
+
 function App() {
   
   // eslint-disable-next-line no-console
   return (
     <div className="App">
       <header className="App-header">
-        <h1>First Course lesson</h1>
+        <h1 className='name title'>Names List</h1>
+        <h3 className='name'>Peter</h3>
+        <h3 className='name'>jOHN</h3>
+        <h3 className='name'>mARTY</h3>
       </header>
     </div>
   );
