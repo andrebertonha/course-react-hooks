5f24e0db05c8dce0986be65bc0f01f3e35d097b1
André Martins <andrebertonhadev@gmail.com>
2020-04-23 22:10:41 -0300

    use state hook class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Input from './components/Input';
 
 function App() {
   
+  const [age, setAge] = useState(21);
+
+  const ageUpHandle = () => {
+    setAge( age + 1 );
+  }
+
+  const ageDownHandle = () => {
+    setAge( age -1 );
+  }
+
   // eslint-disable-next-line no-console
   return (
     <div className="App">
       <header className="App-header">
         <Input placeholder="Enter Here" type="text"></Input>
+        <h1>Use State Hook</h1>
+        <h2>Age: {age}</h2>
+        <button onClick={ageUpHandle}>age up</button>
+        <button onClick={ageDownHandle}>age down</button>
       </header>
     </div>
   );
--- src/components/nameTag.js
+++ src/components/nameTag.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 
-function nameTag(props){
+function nameTag(props) {
+  if (!props.firstName && !props.lastName) {
+    return (
+      <div className="name">
+        <h3>Invalid name</h3>
+      </div> 
+    );
+    
+  }
+
   return (
-    <div className='name'>
+    <div className="name">
       <h3>First name: {props.firstName}</h3>
       <h3>Last name: {props.lastName}</h3>
+      {
+        (props.firstName === 'John' && <div style={{color:'green'}} />)
+      }
     </div>
-  )
+  );
+
 }
 
 export default nameTag;
